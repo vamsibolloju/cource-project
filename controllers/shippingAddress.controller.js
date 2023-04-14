490e4d63230515614a74cc7bf371bcb7f5e08922
Vamsi Krishna <vamsikrishnaa.b88@gmail.com>
2023-04-14 19:11:46 +0530

    send user in the response of new address

--- controllers/shippingAddress.controller.js
+++ controllers/shippingAddress.controller.js
@@ -1,18 +1,14 @@
 const Address = require("../models/address.model");
-const User = require("../models/auth.model");
 
 const address = async (req, res) => {
     const addressRec =  new Address({ ...req.body });
     let addressObj;
     try{
         addressObj = await addressRec.save();
-        console.log(addressObj);
     } catch(err) {
         return res.status(400).end(err.message);
     }
-    const user = await User.findOne({ _id: req.userId });
-
-    return res.status(200).json({...addressObj});
+    return res.status(200).json({...addressObj._doc, user: req.user});
 }
 
 module.exports = {
